c60293541c99e4a2e5f0983c3474d88783e82a61
iNJUSTiCE <injusticenubs@gmail.com>
2024-04-29 15:14:11 +0530

    fivth

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 
-	Addr := &ApiServer{ListenAddr: "127.0.0.1:80", InstanceOfReading: &Reading{}}
+	Addr := &ApiServer{ListenAddr: "0.0.0.0:80", InstanceOfReading: &Reading{}}
 	Runserver(Addr)
 
 }
